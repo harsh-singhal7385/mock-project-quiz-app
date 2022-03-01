36afdecfd7223468880dcbf3ed94cdcff28e153c
harsh-singhal7385 <harsh-singhal7385@github.com>
2022-03-02 02:56:39 +0530

    initial setup complete

---
+++ quiz/quiz_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QuizAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'quiz_admin'
---
+++ quiz/quiz_user/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QuizUserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'quiz_user'
