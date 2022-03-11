439ecb6a5f41fdad711b4968dd8e3d99cf95afa6
harsh-singhal7385 <harsh-singhal7385@github.com>
2022-03-11 10:05:22 +0530

    max possible done

--- quiz/quiz_admin/models.py
+++ quiz/quiz_admin/models.py
@@ -10,14 +10,13 @@ class AddQuiz(models.Model):
     quiz_time = models.IntegerField(null=True)
     date = models.DateField()
     # desc = models.TextField(null=True, blank=False)
-
+    
     def __str__(self):
         return self.subject_name + "_" + self.quizId
 
 
 class Question(models.Model):
-    quizId = models.ForeignKey(
-        AddQuiz, default=None, null=False, blank=False, on_delete=models.CASCADE)
+    quizId = models.ForeignKey( AddQuiz, default=None, null=False, blank=False, on_delete=models.CASCADE )
     qno = models.CharField(max_length=3, null=True, blank=False)
     question = models.TextField(null=True, blank=False)
     option_a = models.TextField(null=True, blank=False)
@@ -26,6 +25,32 @@ class Question(models.Model):
     option_d = models.TextField(null=True, blank=False)
     marks = models.IntegerField(null=True)
     answer = models.CharField(max_length=1, null=True)
-
+    
     def __str__(self):
         return str(self.quizId) + "_" + str(self.qno)
+
+
+###############################################################################
+###demo###
+
+class AddCategory(models.Model):
+    category = models.TextField(max_length=300, null=True)
+    datetime = models.DateTimeField(auto_now_add = True)
+    # desc = models.TextField(null=True, blank=False)
+    def __str__(self):
+        return self.category 
+
+class QuestionDemo(models.Model):
+    # quizId = models.ForeignKey( AddQuiz, default=None, null=False, blank=False, on_delete=models.CASCADE )
+    qno = models.CharField(max_length=3, null=True, blank=False)
+    question = models.TextField(null=True, blank=False)
+    option_a = models.TextField(null=True, blank=False)
+    option_b = models.TextField(null=True, blank=False)
+    option_c = models.TextField(null=True, blank=False)
+    option_d = models.TextField(null=True, blank=False)
+    subject_name = models.CharField( max_length = 300, null = True )
+    marks = models.IntegerField(null=True)
+    answer = models.CharField( max_length=1, null=True )
+     
+    def __str__(self):
+        return str(self.subject_name) + "_" + str(self.qno)
--- quiz/quiz_user/models.py
+++ quiz/quiz_user/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class LeaderBoard(models.Model):
+    index = models.AutoField(primary_key = True)
+    subject_name = models.CharField(max_length=300, null=True)
+    user_name = models.CharField(max_length=300, null=True)
+    category = models.CharField(max_length=300, null=True)
+    quiz_time = models.IntegerField(null=True)
+    score = models.CharField(max_length=300, null=True)
+    maxscore = models.CharField(max_length=300, null=True)
+    datetime = models.DateTimeField(auto_now_add = True)
+    # desc = models.TextField(null=True, blank=False)
+    
+    def __str__(self):
+        return self.subject_name + "_" + self.maxscore
+
+
+class ScoreCard(models.Model):
+    index = models.AutoField(primary_key = True)
+    subject_name = models.CharField(max_length=300, null=True)
+    score = models.CharField(max_length=300, null=True)
+    maxscore = models.CharField(max_length=300, null=True)
+    result = models.CharField(max_length=300, null=True)
+    datetime = models.DateTimeField(auto_now_add = True)
+    # desc = models.TextField(null=True, blank=False)
+    
+    def __str__(self):
+        return self.subject_name + "_" + self.score
